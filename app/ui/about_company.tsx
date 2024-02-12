d3b54a3c744140f77bb6d59e7b8de5bd46d6ba1b
Olanrewaju <lanrebamgbade@gmail.com>
2024-02-12 02:09:39 -0800

    add cinference meeting image

--- app/ui/about_company.tsx
+++ app/ui/about_company.tsx
@@ -6,21 +6,35 @@ import SimpleSlider from "./partners_slider";
 import SmilingWoman from "../assets/images/woman.png";
 import FrequentlyAskedQuestions from "./frequently_asked_questions";
 import Unleash from "./unleash";
+import dashesTwo from "../assets/icons/dashes-2.svg";
+import happyEmployees from "../assets/images/happy-employees.png";
 
 function Company() {
   return (
     <section className="pt-24 w-full font-montserrat">
-      <div className="w-[35%] pl-0 h-48 bg-gradient-to-br blur from-blue-700 via-white to-white rounded-tr-3xl   opacity-25"></div>
-      <div className="-mt-16 bg-white w-[90%] mx-auto flex justify-between items-center">
-        <div className="w-[55%] font-montserrat">
+      <div className="flex justify-between w-full">
+        {" "}
+        <div className="lg:block lg:w-[35%] w-1/2 pl-0 h-48 bg-gradient-to-br blur from-blue-700 via-white to-white rounded-tr-3xl   opacity-25"></div>
+        <Image
+          src={dashes}
+          alt="icon"
+          width="100"
+          height="100"
+          className="ml-auto rotate-180 mb-auto  lg:hidden w-10 h-10 "
+        />
+      </div>
+      <div className="lg:-mt-16 -mt-28 bg-white w-[90%] mx-auto  flex flex-col lg:flex-row justify-between items-center">
+        <div className="lg:w-[55%] lg:mt-0 w-full font-montserrat flex flex-col items-center lg:items-start">
           <div className="rounded-full bg-light-blue w-fit px-3 py-1">
-            <p className="text-dark-blue">Where brilliance meets opportunity</p>
+            <p className="text-dark-blue text-center">
+              Where brilliance meets opportunity
+            </p>
           </div>
           <h2 className="text-light-black lg:text-3xl my-4">
             About Our Company
           </h2>
           <h2 className="text-dark-blue lg:text-3xl">DashTalent</h2>
-          <p className="my-4">
+          <p className="my-4 text-center">
             We are a dynamic platform dedicated to bridging the gap between
             ambitious interns and forward-thinking companies. We are devoted to
             empowering the next generation of professionals by connecting them
@@ -28,15 +42,23 @@ function Company() {
             by our team of industry experts.
           </p>
         </div>
-        <div className="w-[40%] h-[65vh] flex">
+        <div className="lg:w-[40%] w-full lg:h-[65vh] h-[40vh] flex mt-10 lg:mt-0">
           <Image
             src={model3}
             alt="Smiling model"
             width="400"
             height="400"
-            className="w-full  h-full object-cover rounded-2xl"
+            className="w-full  h-full object-cover rounded-2xl hidden lg:block"
           />
-          <div className="">
+
+          <Image
+            src={happyEmployees}
+            alt="Smiling model"
+            width="400"
+            height="400"
+            className="w-full  h-full object-cover rounded-2xl  lg:hidden"
+          />
+          <div className="hidden lg:block">
             <Image
               src={dashes}
               alt="icon"
@@ -49,7 +71,7 @@ function Company() {
       </div>
 
       <div className="w-[90%] mx-auto  mt-28">
-        <div className="w-full">
+        <div className="w-full hidden lg:block">
           <Image
             src={boardMeeting}
             height={1000}
@@ -95,7 +117,7 @@ function Company() {
         </div>
       </div>
 
-      <div className="mt-24 w-[90%] mx-auto ">
+      {/* <div className="mt-24 w-[90%] mx-auto ">
         <div>
           <ul className="list-disc pl-4 lg:flex-none flex justify-center lg:justify-start">
             <li className="text-[#2F2F8C] text-sm lg:text-lg">
@@ -108,7 +130,6 @@ function Company() {
           </h2>
         </div>
 
-        {/* Carousel when viewed on mobile */}
         <SimpleSlider />
 
         <div className="w-full justify-between hidden lg:flex mt-8">
@@ -118,9 +139,9 @@ function Company() {
           <div className="bg-[#747474] w-[18%] h-8 "></div>
           <div className="bg-[#747474] w-[18%] h-8 "></div>
         </div>
-      </div>
+      </div> */}
 
-      <div className="w-full bg-[#F4F4F4] mt-20">
+      {/* <div className="w-full bg-[#F4F4F4] mt-20">
         <div className="mx-auto w-[90%] flex justify-between items-center py-28">
           <div className="w-[55%]">
             <div>
@@ -152,10 +173,10 @@ function Company() {
             />
           </div>
         </div>
-      </div>
+      </div> */}
 
-      <FrequentlyAskedQuestions />
-      <Unleash />
+      {/* <FrequentlyAskedQuestions />
+      <Unleash /> */}
     </section>
   );
 }
